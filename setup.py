d0917b59b99c31dc81b85dc0a653499c3e852112
oTree-org <chris@otree.org>
2017-02-17 12:27:08 +0900

    convert README to rst

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,19 +9,18 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='otreechat',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
     description='oTree chat.',
     long_description=README,
-    url='https://www.otree.org/',
+    url='https://github.com/oTree-org/otreechat',
     author='oTree',
     author_email='chris@otree.org',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
